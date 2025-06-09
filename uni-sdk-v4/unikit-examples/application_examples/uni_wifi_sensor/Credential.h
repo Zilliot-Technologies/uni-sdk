1760cc784505a14dfa07ada52607f8dfd293ae43
Punith Salian <>
2025-06-09 10:16:23 +0530

    Clean up of the repo

--- uni-sdk-v4/unikit-examples/application_examples/uni_wifi_sensor/Credential.h
+++
@@ -1,7 +0,0 @@
-#include <stdio.h>
-#include "pico/stdlib.h"
-
-const char* wifi_ssid = "IKPM G5";
-const char* wifi_pswd = "ikp@wifi@2023";
-const char* ubidots_token = "BBFF-NHUTl9Ye2rVG9MdFeXUkVz8fhSxuZc";
-
